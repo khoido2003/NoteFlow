c1a96fe182b2375b7f7c6e2629f52c674b61dcb4
Khoi Do <khoizpro31@gmail.com>
2024-02-22 00:50:11 +0700

    Create schema and api

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,12 +1,30 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
 import { useUser } from "@clerk/clerk-react";
-import { PlusCircle } from "lucide-react";
+import { useMutation } from "convex/react";
 import Image from "next/image";
+
+import { api } from "@/convex/_generated/api";
+
+import { Button } from "@/components/ui/button";
+import { PlusCircle } from "lucide-react";
+import { toast } from "sonner";
 
 const DocumentsPage = () => {
   const { user } = useUser();
+  const create = useMutation(api.documents.create);
+
+  const onCreate = () => {
+    const promise = create({
+      title: "Untitled",
+    });
+
+    toast.promise(promise, {
+      loading: "Creating...",
+      success: "Document created!",
+      error: "Failed to create document!",
+    });
+  };
 
   return (
     <div className="h-full flex flex-col items-center justify-center space-y-4">
@@ -29,7 +47,7 @@ const DocumentsPage = () => {
       <h2 className="text-lg font-medium">
         Welcome to {user?.fullName}&apos;s NoteFlow
       </h2>
-      <Button>
+      <Button onClick={onCreate}>
         <PlusCircle className="h-4 w-4 mr-2" />
         Create a note
       </Button>
--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -6,8 +6,12 @@ import { usePathname } from "next/navigation";
 import { ElementRef, useCallback, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
 import { UserItem } from "./user-item";
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
 
 export const Navigation = () => {
+  const documents = useQuery(api.documents.get);
+
   const pathname = usePathname();
   const isMobile = useMediaQuery("(max-width: 768px)");
 
@@ -127,7 +131,9 @@ export const Navigation = () => {
         </div>
 
         <div className="mt-4">
-          <p>Document</p>
+          {documents?.map((document) => {
+            return <p key={document._id}>{document.title}</p>;
+          })}
         </div>
         {/* Cursor to indicate that can resize  */}
         <div
