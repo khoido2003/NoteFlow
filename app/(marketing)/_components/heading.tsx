942f2588321a005c381fd32a9d1659692573de04
Khoi Do <khoizpro31@gmail.com>
2024-02-21 18:43:12 +0700

    Add Authentication with Clerk and Setup database with Convex

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -1,7 +1,16 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
 
+import { useConvexAuth } from "convex/react";
+import { Spinner } from "@/components/spinner";
+import Link from "next/link";
+import { SignInButton } from "@clerk/clerk-react";
+
 export const Heading = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
+
   return (
     <div className="max-w-3xl space-y-4">
       <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
@@ -12,10 +21,25 @@ export const Heading = () => {
         Note Flow is the connected workspace where <br />
         better, faster work happens.
       </h3>
-      <Button>
-        Enter Note Flow
-        <ArrowRight className="h-4 w-4 ml-2" />
-      </Button>
+
+      {isLoading && (
+        <div className="w-full flex items-center justify-center">
+          <Spinner size="lg" />
+        </div>
+      )}
+      {isAuthenticated && !isLoading && (
+        <Button>
+          <Link href="/documents">Enter Note Flow</Link>
+          <ArrowRight className="h-4 w-4 ml-2" />
+        </Button>
+      )}
+      {!isAuthenticated && !isLoading && (
+        <SignInButton mode="modal">
+          <Button>
+            Get NoteFlow free <ArrowRight className="h-4 w-4 ml-2" />
+          </Button>
+        </SignInButton>
+      )}
     </div>
   );
 };
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://arriving-parrot-87.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
