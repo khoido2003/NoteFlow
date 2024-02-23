74c18b56637db3ab1d2dc01548879cf6efe5c8cf
Khoi Do <khoizpro31@gmail.com>
2024-02-23 11:08:34 +0700

    Creat navbar to change the the document's title

---
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -0,0 +1,5 @@
+const DocumentIdPage = () => {
+  return <div>document</div>;
+};
+
+export default DocumentIdPage;
--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -25,16 +25,18 @@ import { DocumentList } from "./document-list";
 import { toast } from "sonner";
 
 import { ElementRef, useCallback, useEffect, useRef, useState } from "react";
-import { usePathname } from "next/navigation";
+import { useParams, usePathname } from "next/navigation";
 import { useMediaQuery } from "usehooks-ts";
 import { useMutation } from "convex/react";
 import { useSearch } from "@/hooks/use-search";
 import { useSettings } from "@/hooks/use-settings";
+import { Navbar } from "./navbar";
 
 export const Navigation = () => {
   const search = useSearch();
   const settings = useSettings();
   const create = useMutation(api.documents.create);
+  const params = useParams();
 
   const pathname = usePathname();
   const isMobile = useMediaQuery("(max-width: 768px)");
@@ -214,15 +216,19 @@ export const Navigation = () => {
           isMobile && "left-0 w-full"
         )}
       >
-        <nav className="bg-transparent px-3 py-2 w-full">
-          {isCollapsed && (
-            <MenuIcon
-              onClick={resetWidth}
-              role="button"
-              className="h-6 w-6 text-muted-foreground"
-            />
-          )}
-        </nav>
+        {!!params.documentId ? (
+          <Navbar isCollapsed={isCollapsed} onResetWidth={resetWidth} />
+        ) : (
+          <nav className="bg-transparent px-3 py-2 w-full">
+            {isCollapsed && (
+              <MenuIcon
+                onClick={resetWidth}
+                role="button"
+                className="h-6 w-6 text-muted-foreground"
+              />
+            )}
+          </nav>
+        )}
       </div>
     </>
   );
